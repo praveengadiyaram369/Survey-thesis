ec689642d5d36c35b9c9c80d86ef7667e37a5840
Gadiyaram <sri.sai.praveen.gadiyaram@FKIE>
2022-07-11 10:08:42 +0200

    adding changes for db primary key issue

--- labelling_api/db_utils.py
+++ labelling_api/db_utils.py
@@ -9,7 +9,7 @@ def insert_into_sqlite_db(doc_id, query, label):
     sqlite_common_query_seq(insert_table_query, sql_select=False, sql_insert_params=(query, doc_id, int(label)))
     get_db_contents()
 
-    logging.info('finished inserting into the table: {len()}')
+    logging.info(f'finished inserting into the table: query: {query}, label:{label}')
 
 
 def create_table():
@@ -17,7 +17,8 @@ def create_table():
     create_table_query = """CREATE TABLE IF NOT EXISTS retrieval_dataset (
         query text,
         doc_id text,
-        label integer
+        label integer,
+        PRIMARY KEY (query, doc_id)
     );"""
     sqlite_common_query_seq(create_table_query)
     
@@ -26,7 +27,7 @@ def get_db_contents():
 
     select_table_query = """SELECT * FROM retrieval_dataset"""
     query_result = sqlite_common_query_seq(select_table_query, sql_select=True)   
-    logging.info(query_result)
+    # logging.info(query_result)
     logging.info(f'Total records in the DB: {len(query_result)}')
     
 
