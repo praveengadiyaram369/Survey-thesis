e1b98927c1cac3ea2945fb4eab577bf56aac6b1c
Gadiyaram <sri.sai.praveen.gadiyaram@FKIE>
2022-09-19 12:30:50 +0200

    adding changes for rendering sub-topics

--- labelling_api/app.py
+++ labelling_api/app.py
@@ -2,7 +2,7 @@ from re import sub
 import time
 from unittest import result
 import uvicorn
-from fastapi import FastAPI, Request, Form, render_template
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from elasticsearch import helpers, Elasticsearch
@@ -40,6 +40,7 @@ tech_relevant_document_data = None
 milt_relevant_document_data = None
 irrelevant_document_data = None
 sub_topics_dict = dict()
+sub_topic_list = []
 topic_dict = dict()
 
 @app.get("/")
@@ -168,7 +169,8 @@ async def get_sub_topics(request: Request, query: str=Form(...)):
     global topic_dict
     topic_dict = get_subtopic(results, query)
     sub_topics = list(topic_dict.keys())
-    global sub_topics_dict
+    global sub_topics_dict, sub_topic_list
+    sub_topics_dict = dict()
     sub_topic_list = []
 
     for idx, topic in enumerate(sub_topics):
@@ -201,7 +203,7 @@ async def keyword_search(request: Request, query: str=Form(...), sub_topic_selec
 
     total_hits, results = get_topic_documents_clustering(doc_id_list)
 
-    return templates.TemplateResponse('sub_topic_search.html', context={'request': request, 'total_hits': total_hits, 'result_list': results, 'concept_list': [], 'query': query, 'search_data':search_data, 'sub_topic_list':list(topic_dict.keys())})
+    return templates.TemplateResponse('sub_topic_search.html', context={'request': request, 'total_hits': total_hits, 'result_list': results, 'concept_list': [], 'query': query, 'search_data':search_data, 'sub_topic_list':sub_topic_list})
     
 @app.post('/get_cdd_pool')
 async def get_cdd_pool(request: Request, query: str=Form(...), lang: int=Form(3), phrase_query: bool=Form(False), search_concept: bool=Form(False), match_top: int=Form(...), fuzzy_query: str=Form(False), search_type: str=Form(...)):
