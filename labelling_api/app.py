7956f925160a7caa366d3be87c6364858bab50cb
Gadiyaram <sri.sai.praveen.gadiyaram@FKIE>
2023-02-27 11:51:06 +0100

    adding changes for web

--- labelling_api/app.py
+++ labelling_api/app.py
@@ -156,9 +156,9 @@ async def search_survey(request: Request):
     query_keyword_list, query_keywords_dict = get_keyword_query_data()
 
     global session_id
-    session_id = uuid.uuid4() 
+    session_id = str(uuid.uuid4().hex)
 
-    return templates.TemplateResponse('search_survey.html', context={'request': request, 'total_hits': 0, 'result_list': [], 'concept_list': [], 'search_data': dict(), 'sub_topic_list':[], 'query_keyword_list': query_keyword_list})
+    return templates.TemplateResponse('search_survey.html', context={'request': request,'session_id': session_id,  'total_hits': 0, 'result_list': [], 'concept_list': [], 'search_data': dict(), 'sub_topic_list':[], 'query_keyword_list': query_keyword_list})
 
 @app.get("/search_subtopic")
 async def search_subtopic(request: Request):
@@ -244,7 +244,7 @@ async def keyword_search(request: Request, query: int=Form(1), sub_topic_selecte
 
     total_hits, results = get_topic_documents_clustering(doc_id_list)
 
-    return templates.TemplateResponse('sub_topic_search.html', context={'request': request, 'total_hits': total_hits, 'result_list': results, 'concept_list': [], 'query': query, 'search_data':search_data, 'sub_topic_list':sub_topic_list})
+    return templates.TemplateResponse('sub_topic_search.html', context={'request': request,'session_id': session_id, 'total_hits': total_hits, 'result_list': results, 'concept_list': [], 'query': query, 'search_data':search_data, 'sub_topic_list':sub_topic_list})
 
 
 @app.post('/sub_topic_search_survey')
@@ -279,7 +279,7 @@ async def keyword_search(request: Request, query: str=Form(...), sub_topic_selec
 
     total_hits_semantic, system_b_results = get_query_result_semantic_survey(query_updated, match_top, cut_off=0.75)
 
-    return templates.TemplateResponse('search_survey.html', context={'request': request, 'total_hits': total_hits, 'result_list_1': system_a_results, 'result_list_2': system_b_results, 'concept_list': [], 'query': query, 'search_data':search_data, 'sub_topic_list':sub_topic_list, 'query_keyword_list': query_keyword_list})
+    return templates.TemplateResponse('search_survey.html', context={'request': request,'session_id': session_id, 'total_hits': total_hits, 'result_list_1': system_a_results, 'result_list_2': system_b_results, 'concept_list': [], 'query': query, 'search_data':search_data, 'sub_topic_list':sub_topic_list, 'query_keyword_list': query_keyword_list})
 
 @app.post('/get_cdd_pool')
 async def get_cdd_pool(request: Request, query: str=Form(...), lang: int=Form(3), phrase_query: bool=Form(False), search_concept: bool=Form(False), match_top: int=Form(...), fuzzy_query: str=Form(False), search_type: str=Form(...)):
--- labelling_api/survey_db_utils.py
+++ labelling_api/survey_db_utils.py
@@ -10,7 +10,7 @@ def insert_clustering_output_label(session_id, query, survey_label_chosen):
     sqlite_common_query_seq(insert_table_query, sql_select=False, sql_insert_params=(session_id, query, int(survey_label_chosen), datetime.now()))
 
     select_table_query = """SELECT session_id, query, survey_label_chosen  FROM clustering_output_survey_data where session_id= ? and query= ?"""
-    get_db_contents(select_table_query, (session_id, query,))
+    get_db_contents(select_table_query, [session_id, query])
 
     logging.info(f'finished inserting into the table: query: {query}, label:{survey_label_chosen}')
 
@@ -22,7 +22,7 @@ def insert_system_comparision_label(session_id, query, sub_topic, survey_label_c
     sqlite_common_query_seq(insert_table_query, sql_select=False, sql_insert_params=(session_id, query, sub_topic, int(survey_label_chosen), datetime.now()))
     
     select_table_query = """SELECT session_id, query, sub_topic, survey_label_chosen FROM system_comparision_survey_data where session_id= ? and query= ? and sub_topic= ?"""
-    get_db_contents(select_table_query, (session_id, query, sub_topic,))
+    get_db_contents(select_table_query, [session_id, query, sub_topic])
 
     logging.info(f'finished inserting into the table: query: {query}, sub_topic: {sub_topic}, label:{survey_label_chosen}')
 
@@ -35,7 +35,7 @@ def create_survey_tables():
         query text NOT NULL,
         survey_label_chosen integer NOT NULL, 
         date_modified timestamp,
-        PRIMARY KEY (query, user_id)
+        PRIMARY KEY (query, session_id)
     );"""
     sqlite_common_query_seq(create_table_query_1)
 
@@ -45,7 +45,7 @@ def create_survey_tables():
         sub_topic text NOT NULL,
         survey_label_chosen integer NOT NULL, 
         date_modified text,
-        PRIMARY KEY (query, sub_topic, user_id)
+        PRIMARY KEY (query, sub_topic, session_id)
     );"""
     sqlite_common_query_seq(create_table_query_2)
     logging.info('finished create table ........... ')
@@ -54,7 +54,7 @@ def create_survey_tables():
 def get_db_contents(select_table_query, data):
 
     logging.info(select_table_query)
-
+    query_result = None
     
     try:
         conn = sqlite3.connect(survey_sqlite_db_path)
